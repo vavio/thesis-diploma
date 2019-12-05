112d1d89ea03b93cb7d1508023d09fa8b3b6e9ad
maznikardenis <58091092+maznikardenis@users.noreply.github.com>
2019-12-05 23:13:23 +0100

    Add files via upload

---
+++ codecpp/lang/en/qtype_codecpp.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_codecpp', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package    qtype
+ * @subpackage codecpp
+ * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['correctanswer'] = 'Correct answer';
+$string['correctanswerfalse'] = 'The correct answer is \'False\'.';
+$string['correctanswertrue'] = 'The correct answer is \'True\'.';
+$string['false'] = 'False';
+$string['feedbackfalse'] = 'Feedback for the response \'False\'.';
+$string['feedbacktrue'] = 'Feedback for the response \'True\'.';
+$string['pleaseselectananswer'] = 'Please select an answer.';
+$string['selectone'] = 'Select one:';
+$string['true'] = 'True';
+$string['pluginname'] = 'CodeCPP';
+$string['pluginname_help'] = 'In response to a question (that may include an image) the respondent gives an answer.';
+$string['pluginname_link'] = 'question/type/codecpp';
+$string['pluginnameadding'] = 'Adding a CodeCPP question';
+$string['pluginnameediting'] = 'Editing a CodeCPP question';
+$string['pluginnamesummary'] = 'A simple form of a question which generates unique questions based on the given template';
+$string['privacy:metadata'] = 'The CodeCPP question type plugin does not store any personal data.';
---
+++ codecpp/renderer.php
@@ -0,0 +1,94 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * True-false question renderer class.
+ *
+ * @package    qtype
+ * @subpackage codecpp
+ * @copyright  2009 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ * Generates the output for CodeCPP questions.
+ *
+ * @copyright  2009 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+class qtype_codecpp_renderer extends qtype_renderer {
+    public function formulation_and_controls(question_attempt $qa,
+            question_display_options $options) {
+
+        $question = $qa->get_question();
+        $currentanswer = $qa->get_last_qt_var('answer');
+        $currenttext = $qa->get_last_qt_var('_qtext_');
+        $qa->questiontext = $currenttext;
+
+        $inputname = $qa->get_qt_field_name('answer');
+        $inputattributes = array(
+            'type' => 'text',
+            'name' => $inputname,
+            'value' => $currentanswer,
+            'id' => $inputname,
+            'size' => 30,
+            'class' => 'form-control d-inline',
+        );
+
+        if ($options->readonly) {
+            $inputattributes['readonly'] = 'readonly';
+        }
+
+        $feedbackimg = '';
+        $questiontext = $this->format_newquestiontext($qa, $currenttext);
+        $input = html_writer::empty_tag('input', $inputattributes) . $feedbackimg;
+        $result = html_writer::tag('div', $questiontext, array('class' => 'qtext'));
+
+        $result .= html_writer::start_tag('div', array('class' => 'ablock form-inline'));
+        $result .= html_writer::tag('label', get_string('answercolon', 'qtype_numerical'), array('for' => $inputattributes['id']));
+        $result .= html_writer::tag('span', $input, array('class' => 'answer'));
+        $result .= html_writer::end_tag('div');
+        if ($qa->get_state() == question_state::$invalid) {
+            $result .= html_writer::nonempty_tag('div',
+                    $question->get_validation_error(array('answer' => $currentanswer, 'unit' => $selectedunit)),
+                    array('class' => 'validationerror'));
+        }
+
+        return $result;
+    }
+
+    public function format_newquestiontext($qa, $temp){
+        $question = $qa->get_question();
+        return $question->format_text($temp, $this->questiontextformat,
+                $qa, 'question', 'questiontext', $this->id);
+    }
+
+    public function specific_feedback(question_attempt $qa) {
+        return "";
+    }
+
+    public function correct_response(question_attempt $qa) {
+        $question = $qa->get_question();
+        $currentanswer = $qa->get_last_qt_var('_qans_');
+        return "The answer is: " . $currentanswer;
+    }
+}
